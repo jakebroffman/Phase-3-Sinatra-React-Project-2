accc64f840da7e35c84fae3a359add79f4eccf5b
jakebroffman <jake.broffman1088@gmail.com>
2023-08-19 15:25:52 -0400

    debug index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BroswerRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
 );
