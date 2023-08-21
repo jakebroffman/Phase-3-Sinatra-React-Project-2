eab9c57226959500037d00a127a16fc69e342e36
jakebroffman <jake.broffman1088@gmail.com>
2023-08-21 18:58:54 -0400

    code framework for ProductList.js

---
+++ src/components/ProductList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from 'react-router-dom';
+import Product from "./Product";
+import { Container } from "semantic-ui-react"
+
+function ProductList() {
+
+    return();
+}
+
+export default ProductList;
