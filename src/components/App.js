4ce2b07877ea288883d3de4cf0daac99ab8ddce9
jakebroffman <jake.broffman1088@gmail.com>
2023-08-23 16:22:04 -0400

    Add button to App component for AisleForm component

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,34 @@
 
 import React, { useState, useEffect } from "react";
 import { Route, Routes } from 'react-router-dom';
-import NavBar from "./NavBar";
+import AisleLinks from "./AisleLinks";
 import ProductList from "./ProductList"
 
 function App() {
   const [groceryAisles, setGroceryAisles] = useState([]);
+  const [show, toggleShow] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:9292/aisles")
     .then((r) => r.json())
-    .then((aisles) => console.log(aisles))
+    .then((aisles) => setGroceryAisles(aisles))
   }, []);
+
+console.log(groceryAisles)
 
   return (
     <div>
-      <NavBar />
+      <h2>Grocery Store Inventory Tracker</h2>
+      <h3>Scroll to the bottom for the form to add new products to your store's inventory!</h3>
+      <button onClick={() => toggleShow(!show)}>Add An Aisle:</button>
+      {show && <h2>Hello World!</h2>}
+      <AisleLinks groceryAisles={groceryAisles} />
         <Routes>
-          <Route path="/aisle_one" element = {<ProductList groceryAisles = {groceryAisles} />} />
-          <Route path="/aisle_two" element = {<ProductList groceryAisles = {groceryAisles} />} />
-          <Route path="/aisle_three" element = {<ProductList groceryAisles = {groceryAisles} />} />
+          <Route path="/aisles/:id" element = {<ProductList groceryAisles = {groceryAisles} />} />
         </Routes>
     </div>
   );
 }
 
 export default App;
+ 
