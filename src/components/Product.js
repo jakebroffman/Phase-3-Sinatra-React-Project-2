991b29bb40aa987eb5f34b6566841b82b464bc54
jakebroffman <jake.broffman1088@gmail.com>
2023-08-23 19:09:14 -0400

    build product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Card } from "semantic-ui-react"
 
-function Product() {
+function Product({ product }) {
 
 
-    return();
+    return(
+        <Card>
+        <div className="content">
+            <h1>Product #: {product.id}</h1>
+        </div>
+        <div className="product_name">
+            <h1>Product Name: {product.name} </h1>
+        </div>
+        <div className="product_price">
+            <h1 className="header">Price: {product.price}</h1>
+        </div>
+        <div className="aisle_id">Aisle ID: {product.aisle_id}</div>
+    </Card>  
+    );
 }
 
 export default Product;
+
+
