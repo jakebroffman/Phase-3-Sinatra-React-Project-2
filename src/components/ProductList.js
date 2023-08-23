437318cc7e45927f7dcfc0ba6c5b8e212675bf13
jakebroffman <jake.broffman1088@gmail.com>
2023-08-23 19:09:58 -0400

    begin building ProductList component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,27 @@
 import React from "react";
-import React, { useState, useEffect } from "react";
-import { Route, Routes } from 'react-router-dom';
 import Product from "./Product";
-import { Container } from "semantic-ui-react"
+import { Container, Card } from "semantic-ui-react"
 
-function ProductList() {
-
-    return();
+function ProductList({groceryAisles}) {
+    const groceriesToDisplay = groceryAisles.map((aisle) => {
+        return(
+            <div className="product_card" key={aisle.id}>
+                <br></br>
+                <Product className="productCard" products={aisle.products}/>
+                <br></br>
+            </div>
+        )
+    })
+    
+        return (
+            <Container>
+            <h1 className="des">Products in </h1>
+                <Card className="art-card" itemsPerRow={5}>
+                    {groceriesToDisplay}
+                </Card>
+            </Container>
+        );
 }
 
 export default ProductList;
+
