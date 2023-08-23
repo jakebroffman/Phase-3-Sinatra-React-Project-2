ce64cb699b1cb19a8a237d6cf739608b9cb0f25e
jakebroffman <jake.broffman1088@gmail.com>
2023-08-23 19:06:21 -0400

    complete map function to create links

--- src/components/AisleLinks.js
+++ src/components/AisleLinks.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import { React, useState } from "react";
+import { Link, useParams } from 'react-router-dom'
+import ProductList from "./ProductList";
 
 function AisleLinks({groceryAisles}) {
+    const [show, toggleShow] = useState(false);
 
-   const aisleList = groceryAisles.map((aisle) => <Link to>Hello</Link>)
+   
+
+   const aisleList = groceryAisles.map((aisle) => <div><Link to={`/aisles/${aisle.id}`} onClick={() => toggleShow(!show)} key={aisle.id}>Aisle #{aisle.id}</Link>{show && <ProductList />}</div>)
 
     return(
         <div>{aisleList}</div>
