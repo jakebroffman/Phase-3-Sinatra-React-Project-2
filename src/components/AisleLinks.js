068751196adc2709c31fded69edc316deed16180
jakebroffman <jake.broffman1088@gmail.com>
2023-08-24 21:48:31 -0400

    update AisleLinks component

--- src/components/AisleLinks.js
+++ src/components/AisleLinks.js
@@ -2,13 +2,13 @@ import { React, useState } from "react";
 import { Link } from 'react-router-dom'
 import ProductList from "./ProductList";
 
-function AisleLinks({groceryAisles}) {
+function AisleLinks({groceryAisles, setGroceryAisles}) {
     const [show, toggleShow] = useState(false);
 
     
 
 
-   const aisleList = groceryAisles.map((aisle) => <div key= {aisle.id}><Link to={`/aisles/${aisle.id}`} onClick={() => toggleShow(!show)} key={aisle.id}>Aisle #{aisle.id}</Link>{show && <ProductList groceryAisles={groceryAisles} />}</div>)
+   const aisleList = groceryAisles.map((aisle) => <div key= {aisle.id}><Link to={`/aisles/${aisle.id}`} onClick={() => toggleShow(!show)} key={aisle.id}>Aisle #{aisle.id}</Link>{show && <ProductList groceryAisles={groceryAisles} setGroceryAisles={setGroceryAisles} />}</div>)
 
     return(
         <div>{aisleList}</div>
