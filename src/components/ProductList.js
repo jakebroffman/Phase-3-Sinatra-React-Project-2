c9fea0e54f44f80a81741f83b4ebc076916a5f52
jakebroffman <jake.broffman1088@gmail.com>
2023-09-04 11:45:26 -0400

    refactor ProductList component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -9,11 +9,9 @@ function ProductList({ groceryAisles, setGroceryAisles }) {
   const { id } = useParams();
   const aisleId = parseInt(id);
 
-  // Find the selected aisle based on the aisleId
   const selectedAisle = groceryAisles.find((aisle) => aisle.id === aisleId);
 
   if (!selectedAisle) {
-    // Handle the case where the aisle is not found (invalid id, for example)
     return <div>Aisle not found.</div>;
   }
 
@@ -30,89 +28,3 @@ function ProductList({ groceryAisles, setGroceryAisles }) {
 }
 
 export default ProductList;
-
-
-// // import React, { useState } from "react";
-// // import Product from "./Product";
-// // import { Container, Card } from "semantic-ui-react";
-// // import ProductForm from "./ProductForm";
-// //  import { useParams } from "react-router";
-
-
-// // function ProductList({groceryAisles, setGroceryAisles}) {
-
-// //     const [show, toggleShow] = useState(false);
-
-
-// //      let { id } = useParams();
-
-     
-
-// //     const groceryAisle = groceryAisles.map((aisle) => {
- 
-
-    
-       
-       
-        
-
-// //     });
-
-    
-// //         return (
-// //             <div>
-// //                 {groceryAisle}
-// //             </div>
-// //         );
-// // }
-        
-
-
-// // export default ProductList;
-
-// import React, { useState } from "react";
-// import { Container, Card } from "semantic-ui-react";
-// import ProductForm from "./ProductForm";
-// import { useParams } from "react-router-dom";
-// import Product from "./Product"
-
-// function ProductList({ groceryAisles, setGroceryAisles }) {
-//   const { id } = useParams();
-//   const aisleId = parseInt(id);
-
-//   const [showProductForm, setShowProductForm] = useState({});
-
-//   const handleToggleProductForm = (aisleId) => {
-//     setShowProductForm((prev) => ({
-//       ...prev,
-//       [aisleId]: !prev[aisleId],
-//     }));
-//   };
-
-//   return (
-//     <div>
-//       {groceryAisles.map((aisle) => (
-//         <Container key={aisle.id}>
-//           <h1 className="des">Products in aisle: {aisle.aisle_number}</h1>
-//           <button onClick={() => handleToggleProductForm(aisle.id)}>
-//             Add a product to aisle: {aisle.aisle_number}
-//           </button>
-//           {showProductForm[aisle.id] && (
-//             <ProductForm
-//               className="product_form"
-//               setGroceryAisles={setGroceryAisles}
-//               groceryAisles={groceryAisles}
-//               aisleId={aisle.id} // Pass aisleId to the ProductForm
-//             />
-//           )}
-//           <Card className="product-card" itemsperrow={5}>
-//             {/* Pass the aisle data to the Product component */}
-//             <Product aisle={aisle} />
-//           </Card>
-//         </Container>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default ProductList;
